8e5021690a4bef39b972c5e3202ed868b09db81d
Emily Gotschalk <egotschalk@gmail.com>
2017-09-24 20:28:06 -0400

    Updated year 2000 classification comments, export function, and area calculations.

--- GATER.js
+++ GATER.js
@@ -326,48 +326,48 @@ print ('Total Area (ha)', reducerBuffer); //print area to console
 
 
 //********************************************************Year 2000********************************************************
-
-//add a random number column to geometry imports, use seed (1, 2, and 3)
-var newfc2 = newfc.randomColumn('random', 2);
-
-//Call the masked image in the Assets
+//Import the 'Landsat2000' image from your Assets (from year 2000 cloud filtering script above) and call it "imageMasked"
+//Rename imageMasked "masked"
 var masked = imageMasked;
 
-//define your classification samples to incl. newfc2 and the properties to be considered
+//Add a random number column to geometry imports, use seed (1, 2, and 3). This will be used to separate data into testing and training points.
+var newfc2 = newfc.randomColumn('random', 2);
+
+//Define your classification samples to include newfc2 and the properties to be considered (landcover class type and the random number column)
 var samples = masked.sampleRegions({
   collection: newfc2,   
   properties: ['landcover', 'random'], 
   scale: 30 });
 
-//split training points (90% and 10%)  
+//Split "samples" into training and testing points (90% for training and 10% for testing)
 var training2000 = samples.filterMetadata('random', 'less_than', 0.9);
 var testing2000 = samples.filterMetadata('random', 'not_less_than', 0.9);
 
-//only use the 90% for classification
+//Only use the 90% of points for training classifier
 var classifier = ee.Classifier.randomForest(100).train({
  features: training2000, 
   classProperty: 'landcover'});
 
-//apply classifier
+//Apply classifier to "masked" image for year 1995
 var classified = masked.classify(classifier);
 
-//to validate, compare 10% testing points to the classification product in errorMatrix
+//To validate, compare 10% of points reserved for testing to the classification product "classified" in errorMatrix
 var validation2000 = testing2000.classify(classifier);                                     
 var errorMatrix2000 = validation2000.errorMatrix('landcover', 'classification');           
 print('Error Matrix:', errorMatrix2000);
 print('Overall Accuracy:', errorMatrix2000.accuracy());
 print('Kappa Coefficient 2000: ', errorMatrix2000.kappa());
 
-
-//set location and zoom level
+//The following shows instructions on how to view the classification image
+//Set location and zoom level
 Map.setCenter(-80.67012, 25.15795, 10); 
 
-
 //Display either satellite image OR classified layer to reduce loading time. Can also add the GRTS sample grid and vegetation map for creating training points
-//image_masked is used to classify, classified is the image produced from the training points
+//"imageMasked" is used to classify, "classified" is the image produced from the training points
 //Add satellite image map
 Map.addLayer(masked, {bands: ['B5_median', 'B4_median', 'B3_median'], max: 0.3}, 'masked image');
 
+//Creates variable "palette" that lists color selections for landcover types
 var palette = ['000000', //Water - Black
                '4d9221', //Mangrove Forest - dark green
                '61380B', //Freshwater Marsh - brown
@@ -376,39 +376,40 @@ var palette = ['000000', //Water - Black
                'b2182b', //Bare Ground_Developed - Red
               ];
 
-//add classification based on training points
+//Add classification image "classified" based on training points
 Map.addLayer(classified, 
              {min: 0, max: 5, palette: palette}, 'classification');
+
+//Add GRTS Sampling grids used for identifying subset of GRTS_Boundary for creating training and testing points
 Map.addLayer(GRTS_Sample, {color: '#ac8853'}, 'GRTS');
+
+//Add NPS in situ data for training
 Map.addLayer(vegetationMap, {color: '000000'}, 'Vegetation Map');
 
-
-/*
-//export image as .tif file to google drive if desired
+//Export image as .tif to Google Drive
 Export.image.toDrive({
   image: classified,
-  description: '2000',
+  description: 'Classified_2005',
   scale: 30,
   region: parkBoundary
 });
-*/
 
 //Calculate the area in hectares within each GRTS Sampling Grid
 var area = classified.eq([0, 1, 2, 3, 4, 5]).multiply(ee.Image.pixelArea()).divide(10000);
 var reducer = area.reduceRegion({
   reducer: ee.Reducer.sum(),
-  maxPixels: 50000000,
+  geometry: GRTS_Sample,
   scale: 30,
-  geometry: GRTS_Sample
+  maxPixels: 50000000,
 });
 print ('Sampled Grid Area (ha)', reducer);
 
 //Calculate the area within the mangrove buffer region
 var reducerBuffer = area.reduceRegion({
   reducer: ee.Reducer.sum(),
-  maxPixels: 50000000,
+  geometry: GRTS_Boundary,
   scale: 30,
-  geometry: GRTS_Boundary
+  maxPixels: 50000000,
 });
 print ('Total Area (ha)', reducerBuffer);
 
